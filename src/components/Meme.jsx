057716fec217bc998e76203025369ffdd70c65f1
David Pam <niratpam@gmail.com>
2022-10-18 15:50:38 +0100

    get random meme image url on click

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,22 @@
+import memesData from '../memesData';
+
+function getMemeImage() {
+  let memesArray = memesData.data.memes;
+  let randomNumber = Math.floor(Math.random() * memesArray.length);
+  let url = memesArray.at(randomNumber).url;
+  console.log(url);
+}
+
 function Meme() {
   return (
     <main>
-      <form className='form'>
+      <div className='form'>
         <input type='text' className='form--input' placeholder='Top text' />
         <input type='text' className='form--input' placeholder='Bottom text' />
-        <button className='form--button'>Get a new meme image 🖼</button>
-      </form>
+        <button className='form--button' onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
     </main>
   );
 }
