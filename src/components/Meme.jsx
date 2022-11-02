ef1d81c7976ad18c5e5b332917e99bf000dcca0f
David Pam <niratpam@gmail.com>
2022-11-02 05:50:55 +0100

    add top and bottom text to meme image with form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -16,17 +16,45 @@ function Meme() {
     setMeme(m => ({ ...m, randomImage: url }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme(m => {
+      return {
+        ...m,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <main>
       <div className='form'>
-        <input type='text' className='form--input' placeholder='Top text' />
-        <input type='text' className='form--input' placeholder='Bottom text' />
+        <input
+          type='text'
+          className='form--input'
+          placeholder='Top text'
+          name='topText'
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          className='form--input'
+          placeholder='Bottom text'
+          name='bottomText'
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className='form--button' onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
       {meme.randomImage && (
-        <img className='meme--image' src={meme.randomImage} alt='meme image' />
+        <div className='meme'>
+          <img className='meme--image' src={meme.randomImage} alt='meme image' />
+          <h2 className='meme--text top'>{meme.topText}</h2>
+          <h2 className='meme--text bottom'>{meme.bottomText}</h2>
+        </div>
       )}
     </main>
   );
